80ab904cc6088843785b0cc5f9ad15105daf5568
Benjamin Hutchings <161854347+benjamin-hutchings@users.noreply.github.com>
2024-05-13 20:59:41 +0100

    Time: 50 ms (35.74%), Space: 12.5 MB (76.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,10 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        newStr = []
+        l, r = 0, len(s) - 1
 
-        for idx in s:
-            if idx.isalnum():
-                newStr += idx.lower()
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
 
-        return newStr == newStr[::-1]
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or 
+                ord('0') <= ord(c) <= ord('9'))
